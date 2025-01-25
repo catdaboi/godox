7f511d89525954ce46672c2f06b682b677ccde2e
Çat Da Boi <136582076+catdaboi@users.noreply.github.com>
2025-01-25 19:12:13 +0300

    Update light.py

--- light.py
+++ light.py
@@ -1,97 +1,54 @@
-"""Platform for light integration."""
-from __future__ import annotations
-
 import logging
-
+from homeassistant.components.light import LightEntity
 from .godox import GodoxInstance
-import voluptuous as vol
 
-from pprint import pformat
+_LOGGER = logging.getLogger(__name__)
 
-# Import the device class from the component that you want to support
-import homeassistant.helpers.config_validation as cv
-from homeassistant.components.light import (SUPPORT_BRIGHTNESS, ATTR_BRIGHTNESS,
-                                            PLATFORM_SCHEMA, LightEntity)
-from homeassistant.const import CONF_NAME, CONF_MAC
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
-
-_LOGGER = logging.getLogger("godox")
-
-# Validation of the user's configuration
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
-    vol.Optional(CONF_NAME): cv.string,
-    vol.Required(CONF_MAC): cv.string,
-})
-
-
-def setup_platform(
-    hass: HomeAssistant,
-    config: ConfigType,
-    add_entities: AddEntitiesCallback,
-    discovery_info: DiscoveryInfoType | None = None
-) -> None:
-    """Set up the Godox Light platform."""
-    # Add devices
-    _LOGGER.info(pformat(config))
-    
-    # Cihaz bilgilerini tanımla
-    lights = config.get('lights', [])
-    
-    # Godox ışıklarını ekle
-    add_entities([GodoxLight(light) for light in lights])
+async def async_setup_entry(hass, config_entry, async_add_devices):
+    """Set up the Godox light from a config entry."""
+    mac_address = config_entry.data["mac_address"]
+    device = GodoxInstance(mac_address)
+    async_add_devices([GodoxLight(device)])
 
 class GodoxLight(LightEntity):
-    """Representation of an Godox Light."""
+    """Representation of a Godox Light."""
 
-    def __init__(self, light) -> None:
-        """Initialize an GodoxLight."""
-        _LOGGER.info(pformat(light))
-        self._light = GodoxInstance(light["mac"])
-        self._name = light["name"]
-        self._state = None
-        self._brightness = None
+    def __init__(self, device: GodoxInstance):
+        """Initialize the light."""
+        self._device = device
+        self._name = f"Godox {device.mac}"
+        self._is_on = False
+        self._brightness = 255
 
     @property
-    def name(self) -> str:
-        """Return the display name of this light."""
+    def name(self):
+        """Return the name of the light."""
         return self._name
 
     @property
-    def brightness(self):
-        """Return the brightness of the light.
+    def is_on(self):
+        """Return if the light is on."""
+        return self._is_on
 
-        This method is optional. Removing it indicates to Home Assistant
-        that brightness is not supported for this light.
-        """
+    @property
+    def brightness(self):
+        """Return the brightness of the light."""
         return self._brightness
 
-    @property
-    def supported_features(self):
-        return SUPPORT_BRIGHTNESS
+    async def turn_on(self, **kwargs):
+        """Turn the light on."""
+        await self._device.turn_on()
+        self._is_on = True
+        _LOGGER.info(f"Turned on light: {self._name}")
 
-    @property
-    def is_on(self) -> bool | None:
-        """Return true if light is on."""
-        return self._state
+    async def turn_off(self, **kwargs):
+        """Turn the light off."""
+        await self._device.turn_off()
+        self._is_on = False
+        _LOGGER.info(f"Turned off light: {self._name}")
 
-    async def async_turn_on(self, **kwargs: Any) -> None:
-        """Instruct the light to turn on."""
-        
-        if ATTR_BRIGHTNESS in kwargs:
-            await self._light.set_brightness(kwargs.get(ATTR_BRIGHTNESS, 255))
-            
-        await self._light.turn_on()
-
-    async def async_turn_off(self, **kwargs: Any) -> None:
-        """Instruct the light to turn off."""
-        await self._light.turn_off()
-
-    def update(self) -> None:
-        """Fetch new state data for this light.
-
-        This is the only method that should fetch new data for Home Assistant.
-        """
-        self._state = self._light.is_on
-        self._brightness = self._light.brightness
+    async def set_brightness(self, brightness: int):
+        """Set the brightness of the light."""
+        await self._device.set_brightness(brightness)
+        self._brightness = brightness
+        _LOGGER.info(f"Set brightness of light: {self._name} to {brightness}")
